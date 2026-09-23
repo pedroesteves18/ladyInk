aa862e98c33f890bb6349311361f91b8f91a74f3
Pedro Esteves <pedrolol264@gmail.com>
2026-09-23 11:31:14 -0300

    ajustes nas permissões e lógica de token

--- api/src/middlewares/hasPermission.js
+++ api/src/middlewares/hasPermission.js
@@ -4,22 +4,22 @@ import jwt from "jsonwebtoken";
 const hasPermission = async (req,res,next) => {
     try{
         const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ error: 'Unauthorized' });
+        if(!token){
+            req.user = { role: 'guest' };
+        } else {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded;
         }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        const url = req.url.split("/")[2];
+        const baseUrl = req.url.split("/")[1];
         const userRole = req.user.role;
-        const url = req.baseUrl.split('/')[1];
-        if (PERMISSIONS[url] && (PERMISSIONS[url][req.method.toLowerCase()].includes(userRole))) {
-            if(req.user.id !== req.body.userId && userRole !== 'admin') {
-                return res.status(403).json({ error: 'Insufficient permissions' });
-            }
+        if (PERMISSIONS[baseUrl][req.method.toLowerCase()].includes(userRole) && (userRole === 'admin' || PERMISSIONS[baseUrl][req.method.toLowerCase()].includes('guest'))) {
             next();
         } else {
             return res.status(403).json({ error: 'Insufficient permissions' });
         }
     }catch(error){
+        console.error(error)
         return res.status(500).json({ error: 'Internal server error' });
     }
 };
